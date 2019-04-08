df09be238a77976d755d64d5c8735c8f22539d69
yvan <y.douenel@coopernet.fr>
2019-04-08 16:29:08 +0200

    Correction mise à jour : création des cartes et modification possible.

--- correction/carte.js
+++ correction/carte.js
@@ -4,11 +4,39 @@ class Carte {
     this.question = question;
     this.reponse = reponse;
 
-    // création des élément du DOM
+    // création des éléments du DOM
     this.elementArticle = document.createElement("article");
+
     this.elementQuestion = document.createElement("p");
     this.elementReponse = document.createElement("p");
+
+    this.questionForm = document.createElement("input");
+    this.reponseForm = document.createElement("input");
+
+    // Gestion des événements
+    this.questionForm.onchange = function() {
+      // changement de la valeur de la propriété question
+      this.question = this.questionForm.value;
+
+      // on change la valeur du texte de l'élément elementQuestion,
+      // c'est à dire de l'élément du DOM correspondant à cette question
+      this.elementQuestion.textContent = this.question;
+    }.bind(this);// la méthode bind permet de lier à la fonction de
+    // gestion de l'événement le this qui est ici la carte
+
+    this.reponseForm.onchange = function() {
+      // changement de la valeur de la propriété reponse
+      this.reponse = this.reponseForm.value;
+
+      // on change la valeur du texte de l'élément elementReponse,
+      // c'est à dire de l'élément du DOM correspondant à cette réponse
+      this.elementReponse.textContent = this.reponse;
+    }.bind(this);// la méthode bind permet de lier à la fonction de
+    // gestion de l'événement le this qui est ici la carte
   }
+
+  /** création de la méthode drawCarte qui est ajoutée au
+   * prototype du constructeur de Carte */
   drawCarte() {
     // création de l'élément texte de la question
     let textQuestion = document.createTextNode(this.question);
@@ -21,6 +49,17 @@ class Carte {
     this.elementArticle.appendChild(this.elementQuestion);
     this.elementArticle.appendChild(this.elementReponse);
 
+    // Ajout des éléments du formulaire
+    // on spécifie que l'élément du formulaire est un input text
+    this.questionForm.type = "text";
+    this.reponseForm.type = "text";
+
+    // on ajoute à elementArticle (la carte) les éléments de formulaire
+    this.elementArticle.appendChild(this.questionForm);
+    this.elementArticle.appendChild(this.reponseForm);
+
+    // Enfin on ajoute à la section d'identité "cartes" l'élément du
+    // DOM qui correspond à la carte
     document.getElementById("cartes").appendChild(this.elementArticle);
 
   }
--- correction/main.js
+++ correction/main.js
@@ -1,5 +1,20 @@
-// Récupère l'élément bouton add
-let button_add = document.getElementById("add-carte");
+/**
+ * Création l'élément bouton add <button>Ajouter une carte</button>
+ */
+let button_add = document.createElement("button");
+// Création de l'élément texte du bouton add
+let button_add_text = window.document.createTextNode("Ajouter une carte");
+// Ajouter le texte au bouton
+button_add.appendChild(button_add_text);
+// Ajout du bouton dans le DOM : plus exactement dans le body
+window.document.body.appendChild(button_add);
+
+/**
+ * Création de l'élément <section id="cartes"></section>
+ */
+let cartes = document.createElement("section");
+cartes.setAttribute("id","cartes");
+window.document.body.appendChild(cartes);
 
 // gestion du click sur le bouton add-carte
 button_add.onclick = function() {
@@ -8,4 +23,5 @@ button_add.onclick = function() {
   let carte = new Carte("Question", "Réponse");
   console.log("dessine la carte");
   carte.drawCarte();
+  console.log(carte);
 };
